288b312c0f85b0edcdfd07457ad7fe20e48a1c99
flatironschool <danliu277@gmail.com>
2020-09-11 16:21:00 -0400

    Add solution for 1st problem

---
+++ Two Sum/solution.rb
@@ -0,0 +1,13 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    hash = Hash.new
+    nums.each_with_index do |num, index|
+        if hash[target - num]
+            return [hash[target - num], index]
+        else
+            hash[num] = index
+        end
+    end
+end
