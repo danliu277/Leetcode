b90c8c345f1d50781e31d4b8a47bbbba7ca57165
flatironschool <danliu277@gmail.com>
2020-09-13 16:16:57 -0400

    Problem 7 solution

--- Reverse Integer/solution.rb
+++ Reverse Integer/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer} x
 # @return {Integer}
 def reverse(x)
+    stack = []
+    negative = x < 0 ? true : false
+    x.to_s.reverse.split('').each do |digit|
+        stack.push(digit) if (stack.length == 1 && digit != 0) || digit != '-'
+    end
+    result = stack.reduce{|sum, n| sum+n }.to_i
+    result = result * -1 if negative
     
+    result > (2**31) - 1 || result < -(2**31) ? 0 : result
 end
