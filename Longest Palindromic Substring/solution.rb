5b16c564a0e527ca68fe7e2745c311e0e62e5767
flatironschool <danliu277@gmail.com>
2020-09-12 21:18:05 -0400

    Solution for problem 5

--- Longest Palindromic Substring/solution.rb
+++ Longest Palindromic Substring/solution.rb
@@ -1,5 +1,25 @@
 # @param {String} s
 # @return {String}
 def longest_palindrome(s)
-    
+    return '' if s == nil || s.length == 0
+    start = last = 0
+    for i in 0..s.length - 1 do
+        len1 = expandCenter(s, i, i)
+        len2 = expandCenter(s, i, i+ 1)
+        len = [len1, len2].max
+        if len > (last - start + 1)
+            start = i - ((len - 1) / 2)
+            last = i + (len / 2)
+        end
+    end
+    puts start, last
+    return s[start..last]
+end
+
+def expandCenter(s, left, right) 
+    while left >= 0 && right < s.length && s[left] == s[right] do
+        left-=1
+        right+=1
+    end
+    return right - left - 1
 end
