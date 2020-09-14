f27318c57973b35b62ee36a2e43405049eefa268
flatironschool <danliu277@gmail.com>
2020-09-14 14:35:00 -0400

    Problem 9 solution

--- Palindrome Number/solution.rb
+++ Palindrome Number/solution.rb
@@ -1,5 +1,20 @@
 # @param {Integer} x
 # @return {Boolean}
 def is_palindrome(x)
+    return false if x < 0
     
+    s = x.to_s.split('')
+    result = true
+    
+    while s.length > 1 do
+        if s[0] == s[-1]
+            s.shift
+            s.pop
+        else
+            result = false
+            break
+        end
+    end
+    
+    return result
 end
