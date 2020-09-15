9cb8b5e5083b17bf1013d8bf858e6b4e6b3a5905
flatironschool <danliu277@gmail.com>
2020-09-15 15:49:36 -0400

    Problem 10 solution

--- 10. Regular Expression Matching/solution.rb
+++ 10. Regular Expression Matching/solution.rb
@@ -2,5 +2,13 @@
 # @param {String} p
 # @return {Boolean}
 def is_match(s, p)
-    
+    return s.empty? if p.empty?
+        
+    match = !s.empty? && (p[0] == s[0] || p[0] == '.')
+        
+    if p.length > 1 && p[1] == '*'
+        return is_match(s, p[2..-1]) || (match && is_match(s[1..- 1], p))
+    else
+        return match && is_match(s[1..-1], p[1..-1])
+    end
 end
