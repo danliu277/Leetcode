ea79cc095c0d49f43c927b785d8ab17c75ca1385
flatironschool <danliu277@gmail.com>
2020-09-16 15:45:10 -0400

    Problem 11 solution

--- 11. Container With Most Water/solution.rb
+++ 11. Container With Most Water/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer[]} height
 # @return {Integer}
 def max_area(height)
+    result = left = 0
+    right = height.length - 1
     
+    while left < right do
+        result = [result, [height[left], height[right]].min * (right - left)].max
+        if height[left] < height[right]
+            left += 1
+        else
+            right -= 1
+        end
+    end
+    
+    return result
 end
