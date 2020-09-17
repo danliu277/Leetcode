688f36b45330f794c194a1f14301dec4c95cbf75
flatironschool <danliu277@gmail.com>
2020-09-17 16:25:37 -0400

    Problem 14 solution

--- 14. Longest Common Prefix/solution.rb
+++ 14. Longest Common Prefix/solution.rb
@@ -1,5 +1,22 @@
 # @param {String[]} strs
 # @return {String}
 def longest_common_prefix(strs)
+    result = ''
+    return result if strs.length == 0
+    index = 0
+    length = strs.min_by(&:length).length
+    match = true
     
+    while match && index < length
+        strs.each do |str|
+            if str[index] != strs[0][index]
+                match = false
+            end
+        end
+        break if !match
+        result += strs[0][index]
+        index += 1
+    end
+    
+    return result
 end
