8d37145fc7fcb3f14a9cc7c6084a21d1805a6f89
flatironschool <danliu277@gmail.com>
2020-09-20 13:33:16 -0400

    Problem 16 solution

--- 16. 3Sum Closest/solution.rb
+++ 16. 3Sum Closest/solution.rb
@@ -2,5 +2,26 @@
 # @param {Integer} target
 # @return {Integer}
 def three_sum_closest(nums, target)
+    diff = 1 << 64
+    nums = nums.sort
+    i = 0
     
+    while i < nums.length && diff != 0 do
+        low = i + 1
+        high = nums.length - 1
+        while low < high
+            sum = nums[i] + nums[low] + nums[high]
+            if (target - sum).abs() < diff.abs()
+                diff = target - sum
+            end
+            if sum < target
+                low += 1
+            else
+                high -=1
+            end
+        end
+        i+=1
+    end
+       
+    return target - diff
 end
