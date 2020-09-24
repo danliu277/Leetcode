f24cb6b080d958ea70ec09e4c0c45ccc686e8761
flatironschool <danliu277@gmail.com>
2020-09-24 13:30:54 -0400

    Problem 19 solution

--- 19. Remove Nth Node From End of List/solution.rb
+++ 19. Remove Nth Node From End of List/solution.rb
@@ -10,5 +10,25 @@
 # @param {Integer} n
 # @return {ListNode}
 def remove_nth_from_end(head, n)
+    temp = ListNode.new
+    temp.next = head
+    first = second = temp
+    i = 1
     
+    # Ahead node
+    while i <= n + 1 do
+        first = first.next
+        i += 1
+    end
+    
+    # Both nodes start
+    while first != nil do
+        first = first.next
+        second = second.next
+    end
+    
+    # Target node
+    second.next = second.next.next
+    
+    return temp.next
 end
