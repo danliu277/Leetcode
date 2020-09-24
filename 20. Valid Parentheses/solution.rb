a31f0e55218c82611a9cd4dc3c2418d36b9d3cae
flatironschool <danliu277@gmail.com>
2020-09-24 13:37:01 -0400

    Problem 20 solution

--- 20. Valid Parentheses/solution.rb
+++ 20. Valid Parentheses/solution.rb
@@ -1,5 +1,21 @@
 # @param {String} s
 # @return {Boolean}
 def is_valid(s)
-    
+    stack = []
+    valid = true
+    s.split('').each do |char|
+        if char == '(' || char == '{' || char == '['
+            stack.push(char)
+        elsif stack[stack.length - 1] == '(' && char == ')'
+            stack.pop()
+        elsif stack[stack.length - 1] == '{' && char == '}'
+            stack.pop()
+        elsif stack[stack.length - 1] == '[' && char == ']'
+            stack.pop()
+        else
+            valid = false
+            break
+        end
+    end
+    return valid && stack.length == 0
 end
