5ba658db884669a7668aa77ad4ec53ebbb3a84f2
flatironschool <danliu277@gmail.com>
2020-09-24 13:47:09 -0400

    Problem 21 solution

--- 21. Merge Two Sorted Lists/solution.rb
+++ 21. Merge Two Sorted Lists/solution.rb
@@ -10,5 +10,29 @@
 # @param {ListNode} l2
 # @return {ListNode}
 def merge_two_lists(l1, l2)
+    temp = ListNode.new
+    result = temp
     
+    while l1 != nil && l2 != nil do
+        if l1.val <= l2.val
+            temp.next = ListNode.new(l1.val)
+            l1 = l1.next
+        else
+            temp.next = ListNode.new(l2.val)
+            l2 = l2.next
+        end
+        temp = temp.next
+    end
+    while l1 != nil do
+        temp.next = ListNode.new(l1.val)
+        l1 = l1.next
+        temp = temp.next
+    end
+    while l2 != nil do
+        temp.next = ListNode.new(l2.val)
+        l2= l2.next
+        temp = temp.next
+    end
+    
+    return result.next
 end
