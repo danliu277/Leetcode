d3ccbce5816b4be4a97481a90dc30063e7784db3
flatironschool <danliu277@gmail.com>
2020-09-25 17:53:42 -0400

    Problem 22 solution

--- 22. Generate Parentheses/solution.rb
+++ 22. Generate Parentheses/solution.rb
@@ -1,5 +1,17 @@
 # @param {Integer} n
 # @return {String[]}
 def generate_parenthesis(n)
-    
+    return [''] if n == 0
+    result = []
+    backtrack(result, "", 0, 0, n)
+    return result
+end
+
+def backtrack(result, string, left, right, max)
+    if string.length == max * 2 
+        result.push(string)
+        return
+    end
+    backtrack(result, (string + '('), (left + 1), right, max) if left < max
+    backtrack(result, (string + ')'), left, (right + 1), max) if right < left
 end
