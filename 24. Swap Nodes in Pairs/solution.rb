d9ef50abc02c75b85bafcb1bad281d0f1c2e7ad6
flatironschool <danliu277@gmail.com>
2020-09-29 18:18:53 -0400

    Problem 24 solution

--- 24. Swap Nodes in Pairs/solution.rb
+++ 24. Swap Nodes in Pairs/solution.rb
@@ -9,5 +9,18 @@
 # @param {ListNode} head
 # @return {ListNode}
 def swap_pairs(head)
+    temp = ListNode.new
+    result = swap(head, result)
     
+    return result
+end
+
+def swap(node, result)
+    if node && node.next != nil
+        result = ListNode.new(node.next.val, ListNode.new(node.val))
+        result.next.next = swap(node.next.next, result.next.next)
+    elsif node != nil
+        result = ListNode.new(node.val)
+    end
+    return result
 end
