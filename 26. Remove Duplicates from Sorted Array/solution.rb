d2976865a448bd9dd7a69e60301019c99dbc3323
flatironschool <danliu277@gmail.com>
2020-09-30 17:05:09 -0400

    Problem 26 solution

--- 26. Remove Duplicates from Sorted Array/solution.rb
+++ 26. Remove Duplicates from Sorted Array/solution.rb
@@ -1,5 +1,13 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def remove_duplicates(nums)
-    
+    return 0 if nums.length == 0
+    count = 0
+    nums.each_with_index do |num, index|
+        if nums[count] != num
+            count += 1
+            nums[count] = num
+        end
+    end
+    return count + 1
 end
