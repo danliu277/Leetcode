312c6464eac79a2cb6f7c9de12646c74800a40b5
flatironschool <danliu277@gmail.com>
2020-10-01 15:13:37 -0400

    Problem 27 soultion

--- 27. Remove Element/solution.rb
+++ 27. Remove Element/solution.rb
@@ -2,5 +2,10 @@
 # @param {Integer} val
 # @return {Integer}
 def remove_element(nums, val)
-    
+    index = nums.length - 1
+    nums.reverse_each do |num|
+        nums.delete_at(index) if num == val
+        index -= 1
+    end
+    return nums.length
 end
