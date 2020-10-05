40276a317c23ee3764c0f1e0eb3a1bdc53f92c24
flatironschool <danliu277@gmail.com>
2020-10-05 16:45:00 -0400

    Problem 29 solution

--- 29. Divide Two Integers/solution.rb
+++ 29. Divide Two Integers/solution.rb
@@ -2,5 +2,14 @@
 # @param {Integer} divisor
 # @return {Integer}
 def divide(dividend, divisor)
-    
+    num = (dividend.to_f/divisor).to_s
+    puts num
+    num = num[0, num.index('.')].to_i
+    if num >= 2147483648
+        return 2147483647
+    elsif num <= -2147483648
+        return -2147483648
+    else
+        return num
+    end
 end
