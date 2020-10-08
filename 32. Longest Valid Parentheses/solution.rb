3bd9662d651fc72db069b226e96c7296353f54c2
flatironschool <danliu277@gmail.com>
2020-10-08 16:17:43 -0400

    Problem 32 solution

--- 32. Longest Valid Parentheses/solution.rb
+++ 32. Longest Valid Parentheses/solution.rb
@@ -1,5 +1,19 @@
 # @param {String} s
 # @return {Integer}
 def longest_valid_parentheses(s)
-    
+    result = 0
+    stack = [-1]
+    s.split('').each_with_index do |char, index|
+        if char == '('
+            stack.push(index)
+        else
+            stack.pop()
+            if(stack.empty?)
+                stack.push(index)
+            else
+                result = [result, index - stack.last].max
+            end
+        end
+    end
+    return result
 end
