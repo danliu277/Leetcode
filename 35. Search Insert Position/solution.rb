35d3b437b5813343e25f924690841e4ea7631ac0
flatironschool <danliu277@gmail.com>
2020-10-12 13:54:50 -0400

    Problem 35 solution

--- 35. Search Insert Position/solution.rb
+++ 35. Search Insert Position/solution.rb
@@ -2,5 +2,9 @@
 # @param {Integer} target
 # @return {Integer}
 def search_insert(nums, target)
-    
+    index = 0
+    while index < nums.length && nums[index] < target
+        index += 1
+    end
+    return index
 end
