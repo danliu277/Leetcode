db34b24ab1de64c38bed33c85ccf5da3bbae8558
flatironschool <danliu277@gmail.com>
2020-10-14 13:48:16 -0400

    Problem 38 solution

--- 38. Count and Say/solution.rb
+++ 38. Count and Say/solution.rb
@@ -1,5 +1,30 @@
 # @param {Integer} n
 # @return {String}
 def count_and_say(n)
-    
+    result = '1'
+    return result if n == 1
+    for i in 2..n do
+        puts 
+        j = 0
+        temp = ''
+        current = ''
+        count = 0
+        while j < result.length do
+            if current == ''
+                current = result[j]
+                count = 1
+                j += 1
+            elsif current == result[j]
+                count += 1
+                j += 1
+            else
+                temp += (count.to_s + current)
+                current = ''
+                count = 0
+            end
+        end
+        temp += (count.to_s + current)
+        result = temp
+    end
+    return result
 end
