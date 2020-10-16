4c7c92f19c1b0abba76c8f6a872081a345c1e1e0
flatironschool <danliu277@gmail.com>
2020-10-16 11:14:44 -0400

    Problem 39 solution

--- 39. Combination Sum/solution.rb
+++ 39. Combination Sum/solution.rb
@@ -2,5 +2,21 @@
 # @param {Integer} target
 # @return {Integer[][]}
 def combination_sum(candidates, target)
-    
+    result = []
+    list = []
+    combination(candidates, 0, target, 0, list, result)
+    return result
+end
+
+def combination(candidates, start, target, sum, list, result)
+    return if sum > target
+    if sum == target
+        result.push(list.clone)
+        return
+    end
+    for i in start..(candidates.length - 1)
+        list.push(candidates[i])
+        combination(candidates, i, target, sum + candidates[i], list, result)
+        list.pop()
+    end
 end
