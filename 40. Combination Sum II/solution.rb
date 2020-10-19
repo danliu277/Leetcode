9408b6fff0be95df210ab277ddf9d84923fbac2e
flatironschool <danliu277@gmail.com>
2020-10-19 16:04:27 -0400

    Problem 40 solution

--- 40. Combination Sum II/solution.rb
+++ 40. Combination Sum II/solution.rb
@@ -2,5 +2,26 @@
 # @param {Integer} target
 # @return {Integer[][]}
 def combination_sum2(candidates, target)
-    
+    result = []
+    current = []
+    candidates = candidates.sort
+    combination(candidates, target, current, 0, result)
+    return result
+end
+
+def combination(candidates, target, current, start, result)
+    return if target < 0
+    if target == 0
+        result.push(current.clone)
+        return
+    end
+    prev = -1
+    for i in start..candidates.length - 1
+        if prev != candidates[i]
+            current.push(candidates[i])
+            combination(candidates, target - candidates[i], current, i + 1, result)
+            current.pop()
+            prev = candidates[i]
+        end
+    end
 end
