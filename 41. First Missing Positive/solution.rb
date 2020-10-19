dd94c8f35c9a011a18549349e9b198d3fe6f7d9e
flatironschool <danliu277@gmail.com>
2020-10-19 16:15:05 -0400

    Add problem 41

--- 41. First Missing Positive/solution.rb
+++ 41. First Missing Positive/solution.rb
@@ -1,5 +1,10 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def first_missing_positive(nums)
-    
+    nums = nums.sort
+    result = 1
+    nums.each do |num|
+        result = num + 1 if num == result
+    end
+    return result
 end
