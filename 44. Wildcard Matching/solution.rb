ef7ec045a709cc4a7fcb71cad329b4c7fe243ba5
flatironschool <danliu277@gmail.com>
2020-10-23 14:10:36 -0400

    Problem 44 attempt

--- 44. Wildcard Matching/solution.rb
+++ 44. Wildcard Matching/solution.rb
@@ -2,5 +2,31 @@
 # @param {String} p
 # @return {Boolean}
 def is_match(s, p)
+    return true if s.length == 0 && p.length == 0
+    j = 0
+    start = -1
+    index = -1
     
+    for i in 0..s.length - 1 do
+        if j < p.length && (p[j] == '?' || p[j] == s[i])
+            i+=1
+            j+=1
+        elsif j < p.length && p[j]  == '*'
+            start += j
+            index = i
+            j += 1
+        elsif start != -1
+            j = start + 1
+            i = index + 1
+            index += 1
+        else
+            return false
+        end
+    end
+    
+    while j < p.length && p[j] == '*' do
+        j += 1
+    end
+    
+    return j == p.length
 end
